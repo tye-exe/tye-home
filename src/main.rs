4fa06b51048e98978d3ddd557e6b5f63fab74c61
tye-exe <tye@mailbox.org>
2025-01-21 21:09:27 +0000

    Changed app name to 'tye-home.xyz' on native

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "tye-home.xyz",
         native_options,
         Box::new(|cc| Ok(Box::new(tye_home::App::new(cc)))),
     )
