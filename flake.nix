d968b2518a0750713f590f07de8442585eaa04cd
tye-exe <tye@mailbox.org>
2025-06-23 15:53:54 +0100

    Added nix-flakes for dev environment

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Tye's Website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              minify
+            ];
+          };
+      }
+    );
+}
